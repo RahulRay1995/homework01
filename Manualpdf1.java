1766f6b817fc0d8a16f726fd18eb1a4402f78001
RahulRay1995 <rahulray171995@gmail.com>
2022-12-28 21:13:14 +0000

    first commit

---
+++ Manualpdf1.java
@@ -0,0 +1,16 @@
+package manualtopic;
+
+public class Manualpdf1 {
+    public static void main(String[] args) {
+        System.out.println("1. What is software?");
+        System.out.println("2. What is Software Testing?");
+        System.out.println("3. What are the benefit of software Testing?");
+        System.out.println("4. Who does Testing?");
+        System.out.println("5. Qualification of Tester");
+        System.out.println("6. Challenges of Tester");
+        System.out.println("7. What is manual Testing");
+        System.out.println("8. SDLC Software Development Life Cycle");
+        System.out.println("9. STLC Software Testing Life Cylce");
+        System.out.println("10. Software Risk");
+    }
+}
